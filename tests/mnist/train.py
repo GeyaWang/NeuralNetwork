44770a0a1c5e739a3ba7713177b55dca47e7a068
GeyaWang <geyaewang@gmail.com>
2024-01-31 08:41:43 +0000

    Refactor

--- tests/mnist/train.py
+++ tests/mnist/train.py
@@ -1,16 +1,18 @@
-from keras.datasets import mnist
-from keras.utils import to_categorical
+import mnist
 from nn.models import Sequential
 from nn.layers import Dense, Conv2D, Flatten, Activation
 from nn.activations import ReLU, SoftMax
 from nn.losses import CrossEntropy
 from nn.optimisers import Adam
+import numpy as np
 
 
 def main():
-    (x_train, y_train), _ = mnist.load_data()
-    x_train = x_train.reshape(60000, 28, 28, 1)
-    y_train = to_categorical(y_train)
+    x, y, x_test, y_test = mnist.mnist('MNIST')
+    n, H, W = x.shape
+
+    x_train = np.reshape(x, (n, H, W, 1)) / 255
+    y_train = np.eye(10)[y]
 
     model = Sequential()
     model.add(Conv2D(64, 3, input_shape=(28, 28, 1)))
