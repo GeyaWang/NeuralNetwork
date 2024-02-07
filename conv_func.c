e208852c7d463544320ad9c5d42a6815adfe9ac3
GeyaWang <111749246+GeyaWang@users.noreply.github.com>
2024-02-07 23:17:06 +0000

    Update conv_func.c

--- conv_func.c
+++ conv_func.c
@@ -1,21 +1,18 @@
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
 #include <Python.h>
 #include <numpy/arrayobject.h>
 #include <string.h>
 
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+
 static PyObject *_forward(PyObject* self, PyObject *args) {
-    PyObject *X_obj;
-    PyObject *K_obj;
-    PyObject *B_obj;
+    PyArrayObject *X;
+    PyArrayObject *K;
+    PyArrayObject *B;
     char *padding;
-
-    PyArg_ParseTuple(args, "OOOs", &X_obj, &K_obj, &B_obj, &padding);
-
-    PyArrayObject *X = PyArray_FROM_OTF(X_obj, NPY_FLOAT64, NPY_ARRAY_IN_ARRAY);
-    PyArrayObject *K = PyArray_FROM_OTF(K_obj, NPY_FLOAT64, NPY_ARRAY_IN_ARRAY);
-    PyArrayObject *B = PyArray_FROM_OTF(B_obj, NPY_FLOAT64, NPY_ARRAY_IN_ARRAY);
+    PyArg_ParseTuple(args, "OOOs", &X, &K, &B, &padding);
 
     // Get input dimensions
     int N = PyArray_DIM(X, 0);
@@ -41,10 +38,10 @@ static PyObject *_forward(PyObject* self, PyObject *args) {
     npy_intp dims[] = {N, H2, W2, C2};
     PyArrayObject *Y = PyArray_SimpleNew(4, dims, NPY_DOUBLE);
 
-    double *X_data = (double*)PyArray_DATA(X);
-    double *K_data = (double*)PyArray_DATA(K);
-    double *B_data = (double*)PyArray_DATA(B);
-    double *Y_data = (double*)PyArray_DATA(Y);
+    double *X_data = (double *)PyArray_DATA(X);
+    double *K_data = (double *)PyArray_DATA(K);
+    double *B_data = (double *)PyArray_DATA(B);
+    double *Y_data = (double *)PyArray_DATA(Y);
 
     double sum;
     double bias;
@@ -71,25 +68,15 @@ static PyObject *_forward(PyObject* self, PyObject *args) {
         }
     }
 
-    // Cleanup
-    Py_DECREF(X);
-    Py_DECREF(K);
-    Py_DECREF(B);
-
     return PyArray_Return(Y);
 }
 
 static PyObject *_backward(PyObject* self, PyObject *args) {
-    PyArrayObject *X_obj;
-    PyArrayObject *K_obj;
-    PyArrayObject *dY_obj;
+    PyArrayObject *X;
+    PyArrayObject *K;
+    PyArrayObject *dY;
     char *padding;
-
-    PyArg_ParseTuple(args, "OOOs", &X_obj, &K_obj, &dY_obj, &padding);
-
-    PyArrayObject *X = PyArray_FROM_OTF(X_obj, NPY_FLOAT64, NPY_ARRAY_IN_ARRAY);
-    PyArrayObject *K = PyArray_FROM_OTF(K_obj, NPY_FLOAT64, NPY_ARRAY_IN_ARRAY);
-    PyArrayObject *dY = PyArray_FROM_OTF(dY_obj, NPY_FLOAT64, NPY_ARRAY_IN_ARRAY);
+    PyArg_ParseTuple(args, "OOOs", &X, &K, &dY, &padding);
 
     // Get input dimensions
     int N = PyArray_DIM(X, 0);
@@ -106,17 +93,20 @@ static PyObject *_backward(PyObject* self, PyObject *args) {
     npy_intp dims_dX[] = {N, H1, W1, C1};
     npy_intp dims_dW[] = {k1, k2, C1, C2};
     npy_intp dims_dB[] = {C2};
-
     PyArrayObject *dX = PyArray_SimpleNew(4, dims_dX, NPY_DOUBLE);
     PyArrayObject *dW = PyArray_SimpleNew(4, dims_dW, NPY_DOUBLE);
     PyArrayObject *dB = PyArray_SimpleNew(1, dims_dB, NPY_DOUBLE);
 
-    double *X_data = (double*)PyArray_DATA(X);
-    double *K_data = (double*)PyArray_DATA(K);
-    double *dX_data = (double*)PyArray_DATA(dX);
-    double *dW_data = (double*)PyArray_DATA(dW);
-    double *dB_data = (double*)PyArray_DATA(dB);
-    double *dY_data = (double*)PyArray_DATA(dY);
+    // fill with zeros
+    PyArray_FILLWBYTE(dX, 0);
+    PyArray_FILLWBYTE(dW, 0);
+
+    double *X_data = (double *)PyArray_DATA(X);
+    double *K_data = (double *)PyArray_DATA(K);
+    double *dX_data = (double *)PyArray_DATA(dX);
+    double *dW_data = (double *)PyArray_DATA(dW);
+    double *dB_data = (double *)PyArray_DATA(dB);
+    double *dY_data = (double *)PyArray_DATA(dY);
 
     int pad_dX_x; int pad_dX_y;
     int pad_dW_x; int pad_dW_y;
@@ -131,21 +121,26 @@ static PyObject *_backward(PyObject* self, PyObject *args) {
 
     int ki = k1 - 1; int kj = k2 - 1;
     int n_H1; int n_H2;
+
     int max_x; int max_y;
     double dB_sum; double dW_sum;
     double K_val;
 
     // dB
     for (int c2 = 0; c2 < C2; ++c2) {
+
         dB_sum = 0;
         for (int n = 0; n < N; ++n) {
+
             n_H1 = n * H1;
             n_H2 = n * H2;
+
             for (int h2 = 0; h2 < H2; ++h2) {
                 for (int w2 = 0; w2 < W2; ++w2) {
                     dB_sum += dY_data[((n_H2 + h2) * W2 + w2) * C2 + c2];
                 }
             }
+
             // dX
             for (int c1 = 0; c1 < C1; ++c1) {
                 for (int i = 0; i < k1; ++i) {
@@ -176,11 +171,6 @@ static PyObject *_backward(PyObject* self, PyObject *args) {
         }
         dB_data[c2] = dB_sum;
     }
-
-    // Cleanup
-    Py_DECREF(X);
-    Py_DECREF(K);
-    Py_DECREF(dY);
 
     return Py_BuildValue("OOO", dX, dW, dB);
 }
