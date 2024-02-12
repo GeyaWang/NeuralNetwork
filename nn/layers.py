a0142d55cb6514dfa84acf7b7190e981a972e5db
GeyaWang <geyaewang@gmail.com>
2024-02-12 00:01:20 +0000

    Added pooling layers

--- nn/layers.py
+++ nn/layers.py
@@ -1,12 +1,12 @@
-from ._template import Layer, TrainableLayer, TrainingOnlyLayer, Initialise
+from ._template import Layer, TrainableLayer, TrainingOnlyLayer
 from .initializers import GlorotUniform, Zeros
+from math import floor, ceil
 import numpy as np
-from typing import Literal
 import conv_func
 
 
 class Activation(Layer):
-    def __init__(self, activation: Layer):
+    def __init__(self, activation):
         super().__init__()
 
         self.activation = activation
@@ -20,7 +20,7 @@ class Activation(Layer):
 
 
 class Dense(TrainableLayer):
-    def __init__(self, units: int, input_shape: tuple[int] = None, kernel_initializer: Initialise = GlorotUniform(), bias_initializer: Initialise = Zeros()):
+    def __init__(self, units, input_shape=None, kernel_initializer=GlorotUniform(), bias_initializer=Zeros()):
         super().__init__(input_shape, (units,))
 
         self.weights = None
@@ -50,7 +50,10 @@ class Dense(TrainableLayer):
 
         return dX
 
-    def init_params(self):
+    def init(self):
+        print(self.output_shape)
+
+        # Init params
         kernel_shape = (self.input_shape[0], self.output_shape[0])
         bias_shape = self.output_shape[0]
 
@@ -69,11 +72,11 @@ class Conv2D(TrainableLayer):
     def __init__(
             self,
             filters,
-            kernel_size: int | tuple[int, int],
-            input_shape: tuple[int, int, int] = None,
-            padding: Literal['valid', 'same'] = 'valid',
-            kernel_initializer: Initialise = GlorotUniform(),
-            bias_initializer: Initialise = Zeros()
+            kernel_size,
+            input_shape=None,
+            padding='valid',
+            kernel_initializer=GlorotUniform(),
+            bias_initializer=Zeros()
     ):
         super().__init__(input_shape, None)
 
@@ -114,7 +117,13 @@ class Conv2D(TrainableLayer):
 
         return dX
 
-    def init_params(self):
+    def init(self):
+        # Output shape
+        height = self.input_shape[0] - self.kernel_size[0] + 2 * self.pad[0] + 1
+        width = self.input_shape[1] - self.kernel_size[1] + 2 * self.pad[1] + 1
+        self.output_shape = (height, width, self.filters)
+
+        # Params
         kernel_shape = (*self.kernel_size, self.input_shape[2], self.filters)
         bias_shape = (self.filters,)
 
@@ -128,11 +137,134 @@ class Conv2D(TrainableLayer):
     def params(self):
         return self.weights.size + self.bias.size
 
-    @property
-    def output_shape(self):
-        height = self.input_shape[0] - self.kernel_size[0] + 2 * self.pad[0] + 1
-        width = self.input_shape[1] - self.kernel_size[1] + 2 * self.pad[1] + 1
-        return height, width, self.filters
+
+class MaxPooling2D(Layer):
+    def __init__(self, pool_size=(2, 2), strides=None, padding='valid'):
+        super().__init__()
+
+        if isinstance(pool_size, int):
+            self.pool_size = (pool_size, pool_size)
+        else:
+            self.pool_size = pool_size
+
+        if strides is None:
+            self.strides = self.pool_size
+        elif isinstance(strides, int):
+            self.strides = (strides, strides)
+        else:
+            self.strides = strides
+
+        self.padding = padding
+        self.pad = None  # Initialised at self.init()
+
+        self.cached_X = None
+
+    def forward(self, X):
+        self.cached_X = X
+
+        N, H1, W1, C1 = X.shape
+        H2, W2, _ = self.output_shape
+
+        Y = np.zeros((N, H2, W2, C1))
+
+        for n in range(N):
+            for h in range(H2):
+                for w in range(W2):
+                    for c in range(C1):
+                        max_ = -999
+
+                        for i in range(self.pool_size[0]):
+                            for j in range(self.pool_size[1]):
+                                x_val = X[n, h * self.strides[0] + i, w * self.strides[1] + j, c]
+                                if max_ < x_val:
+                                    max_ = x_val
+
+                        Y[n, h, w, c] = max_
+        return Y
+
+    def backward(self, dY):
+        N, H1, W1, C1 = self.cached_X.shape
+        H2, W2, _ = self.output_shape
+
+        dX = np.zeros((N, H1, W1, C1))
+
+        for n in range(N):
+            for h in range(H2):
+                for w in range(W2):
+                    for c in range(C1):
+                        rel_pos_x = None
+                        rel_pos_y = None
+                        max_ = -999
+
+                        for i in range(self.pool_size[0]):
+                            for j in range(self.pool_size[1]):
+                                x_val = self.cached_X[n, h * self.strides[0] + i, w * self.strides[1] + j, c]
+                                if x_val > max_:
+                                    max_ = x_val
+                                    rel_pos_x = i
+                                    rel_pos_y = j
+
+                        dX[n, h * self.strides[0] + rel_pos_x, w * self.strides[1] + rel_pos_y, c] = dY[n, h, w, c]
+        return dX
+
+    def init(self):
+        if self.padding == 'same':
+            height = ceil(self.input_shape[0] / self.strides[0])
+            width = ceil(self.input_shape[1] / self.strides[1])
+
+            # Calculate padding
+            pad_x = ((height - 1) * self.strides[0] + self.pool_size[0] - self.input_shape[0]) / 2
+            pad_y = ((width - 1) * self.strides[1] + self.pool_size[1] - self.input_shape[1]) / 2
+            self.pad = (pad_x, pad_y)
+
+        else:  # self.padding == 'valid'
+            height = floor((self.input_shape[0] - self.pool_size[0]) / self.strides[0]) + 1
+            width = floor((self.input_shape[1] - self.pool_size[1]) / self.strides[1]) + 1
+
+        self.output_shape = (height, width, self.input_shape[2])
+
+
+class AveragePooling2D(MaxPooling2D):
+    def __init__(self, pool_size=(2, 2), strides=None, padding='valid'):
+        super().__init__(pool_size, strides, padding)
+
+    def forward(self, X):
+        self.cached_X = X
+
+        N, H1, W1, C1 = X.shape
+        H2, W2, _ = self.output_shape
+
+        Y = np.zeros((N, H2, W2, C1))
+
+        for n in range(N):
+            for h in range(H2):
+                for w in range(W2):
+                    for c in range(C1):
+                        sum_ = 0
+
+                        for i in range(self.pool_size[0]):
+                            for j in range(self.pool_size[1]):
+                                sum_ += X[n, h * self.strides[0] + i, w * self.strides[1] + j, c]
+
+                        Y[n, h, w, c] = sum_ / (self.pool_size[0] * self.pool_size[1])
+        return Y
+
+    def backward(self, dY):
+        N, H1, W1, C1 = self.cached_X.shape
+        H2, W2, _ = self.output_shape
+
+        dX = np.zeros((N, H1, W1, C1))
+
+        for n in range(N):
+            for h in range(H2):
+                for w in range(W2):
+                    for c in range(C1):
+                        val = dY[n, h, w, c] / (self.pool_size[0] * self.pool_size[1])
+
+                        for i in range(self.pool_size[0]):
+                            for j in range(self.pool_size[1]):
+                                dX[n, h * self.strides[0] + i, w * self.strides[1] + j, c] = val
+        return dX
 
 
 class Flatten(Layer):
@@ -143,17 +275,16 @@ class Flatten(Layer):
     def forward(self, X):
         self.cached_X_shape = X.shape
 
-        n = X.shape[0]
-        p = np.prod(X.shape[1:])
+        N = X.shape[0]
+        P = np.prod(X.shape[1:])
 
-        return np.reshape(X, (n, p))
+        return np.reshape(X, (N, P))
 
     def backward(self, dY):
         return np.reshape(dY, self.cached_X_shape)
 
-    @property
-    def output_shape(self):
-        return (np.prod(self.input_shape),)
+    def init(self):
+        self.output_shape = (np.prod(self.input_shape),)
 
 
 class Dropout(TrainingOnlyLayer):
