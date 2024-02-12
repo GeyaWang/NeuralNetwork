309e6b331d8a8ace715760589da0fba90774629c
GeyaWang <geyaewang@gmail.com>
2024-02-12 00:07:18 +0000

    Refactor

--- nn/conv_func.pyi
+++ nn/conv_func.pyi
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Literal
 
 def forward(X: np.ndarray, K: np.ndarray, B: np.ndarray, padding: Literal['valid', 'same']) -> np.ndarray:
-    pass
+    ...
 
 def backward(X: np.ndarray, K: np.ndarray, dY: np.ndarray, padding: Literal['valid', 'same']) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    pass
+    ...
