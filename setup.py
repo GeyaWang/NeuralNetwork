6d5a03e7e00970bdb3f7c72d1d4c0fc7d8be2365
GeyaWang <geyaewang@gmail.com>
2024-02-12 11:01:25 +0000

    Added pooling max ext

--- setup.py
+++ setup.py
@@ -8,10 +8,18 @@ conv_ext = Extension(
     ['conv_func.c']
 )
 
+pool_max_ext = Extension(
+    'pooling_max_func',
+    ['pooling_max_func.c']
+)
+
 setup(
     name='nn-func-lib',
-    version='2.7',
+    version='3.0',
     install_requires=['numpy'],
-    ext_modules=[conv_ext],
+    ext_modules=[
+        conv_ext,
+        pool_max_ext
+    ],
     include_dirs=[numpy.get_include()]
 )
