8258a38e2136e9e2fb035c60b2e1bb59c20f3b9f
GeyaWang <geyaewang@gmail.com>
2024-02-12 00:00:46 +0000

    Spelling change

--- nn/initializers.py
+++ nn/initializers.py
@@ -1,16 +1,16 @@
-from ._template import Initialise
+from ._template import Initialize
 from ._parameter import Parameter
 import numpy as np
 
 
-class Zeros(Initialise):
+class Zeros(Initialize):
     @staticmethod
     def __call__(shape, dtype=None, **kwargs):
         param = np.zeros(shape, dtype).view(Parameter)
         return param
 
 
-class GlorotUniform(Initialise):
+class GlorotUniform(Initialize):
     @staticmethod
     def __call__(shape, dtype=None, fan_in=None, fan_out=None):
         limit = np.sqrt(6 / (fan_in + fan_out))
